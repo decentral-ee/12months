eff1954c1f04c39c7c87ea44421aeb38aa91cdf5
Jaanek Oja <jaanek@oja.me>
2019-03-09 19:34:12 +0200

    refactoring, fixes

--- client/src/id-card.js
+++ client/src/id-card.js
@@ -1,6 +1,8 @@
 const hw = window.hwcrypto;
 
 export async function sign(hash) {
+  await hw.use('auto');
+  hw.debug();
   const cert = await hw.getCertificate({lang: 'en'});
   // console.log("Using certificate:\n" + cert);
   const signature = await hw.sign(cert, {type: 'SHA-256', hex: hash}, {lang: 'en'});
